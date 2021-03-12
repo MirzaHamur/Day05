125cc20ab0a814ba53ac419cf0b975536074772e
MirzaHamur <79905575+MirzaHamur@users.noreply.github.com>
2021-03-12 20:15:45 +0100

    Update index.js

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -3,7 +3,7 @@ function main() {
     var myString = "FirstLine\n\t\SecondLine\nThirdLine";
     // Only change code above this line
     return myString;
-};
+}
 console.log(main());
 
 module.exports = main;
